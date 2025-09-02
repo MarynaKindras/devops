3551b4b41b9d90da275eef1e1be0021cd5c21ffa
Maryna Kindras <140345218+MarynaKindras@users.noreply.github.com>
2025-09-02 10:05:20 +0300

    lesson-8-9

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
   }
 }
 
@@ -12,14 +20,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "s3_backend" {
-  source      = "./modules/s3-backend"
-  bucket_name = "marynakindaras-terraform-state-lesson-6"
-  table_name  = "terraform-locks"
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-east-1"]
+  }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-east-1"]
+    }
+  }
+}
 
-# VPC Module
+module "s3_backend" {
+  source      = "./modules/s3-backend"
+  bucket_name = "marynakindras-terraform-state-lesson-6"
+  table_name  = "terraform-locks"
+}
 
 module "vpc" {
   source             = "./modules/vpc"
@@ -27,7 +56,6 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  
   vpc_name           = "lesson-6-vpc"
 }
 
@@ -51,4 +79,20 @@ module "eks" {
   node_group_min_size     = 2
   node_group_max_size     = 6
   node_group_desired_size = 2
+}
+
+module "jenkins" {
+  source = "./modules/jenkins"
+  
+  cluster_name     = module.eks.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+  namespace        = "jenkins"
+}
+
+module "argo_cd" {
+  source = "./modules/argo_cd"
+  
+  cluster_name     = module.eks.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+  namespace        = "argocd"
 }
--- modules/s3-backend/dynamodb.tf
+++ modules/s3-backend/dynamodb.tf
@@ -1,9 +1,7 @@
 resource "aws_dynamodb_table" "terraform_locks" {
-
   name           = var.table_name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
-
 
   attribute {
     name = "LockID"
@@ -15,4 +13,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     Environment = "lesson-6"
   }
 }
-
